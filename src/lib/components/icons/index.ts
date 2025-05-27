85746daf111a5cf395c1dc929f1ea16ead736049
Geoffrey Belcher <geoffreybelcher@Geoffreys-Air.cogeco.local>
2025-05-27 13:55:20 -0400

    Create layout

---
+++ src/lib/components/icons/index.ts
@@ -0,0 +1,2 @@
+export { default as LinkedIn } from './LinkedIn.svelte';
+export { default as Github } from './Github.svelte';
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,32 @@
 <script lang="ts">
-	import { Button } from '$lib/components/ui/button';
+	import { Github, LinkedIn } from '$lib/components/icons';
+	import { buttonVariants } from '$lib/components/ui/button';
+	import * as Card from '$lib/components/ui/card';
 </script>
 
-<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<Button onclick={() => console.log('hello')}>Thingy</Button>
+{#snippet paragraph(str: string)}
+	<p class="text-sm leading-6 font-medium [&:not(:first-child)]:mt-2">{str}</p>
+{/snippet}
+
+<div class="flex h-screen items-center justify-center">
+	<Card.Root class="max-w-lg">
+		<Card.Header>
+			<Card.Title>Geoffrey Belcher</Card.Title>
+			<Card.Description>Software developer living in Toronto.</Card.Description>
+		</Card.Header>
+		<Card.Content>
+			<div class="flex flex-col gap-2">
+				{@render paragraph('I build software in SvelteKit, Rust & IOS.')}
+				{@render paragraph('Open to work, but want an in-person role in Toronto.')}
+			</div>
+		</Card.Content>
+
+		<Card.Footer class="flex items-center justify-end gap-2">
+			<a class={buttonVariants({ variant: 'ghost' })} href="mailto:geoffreybelcher@icloud.com"
+				>Email</a
+			>
+			<LinkedIn />
+			<Github />
+		</Card.Footer>
+	</Card.Root>
+</div>
